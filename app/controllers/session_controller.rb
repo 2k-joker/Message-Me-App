8f2289fbb44339c20baeec6d396427290c9003dd
Khalil Kum <kum.vanjunior@gmail.com>
2020-07-26 19:06:10 -0400

    Add login-signup routes and views

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,4 +2,22 @@ class SessionController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to login_path
+  end
 end
